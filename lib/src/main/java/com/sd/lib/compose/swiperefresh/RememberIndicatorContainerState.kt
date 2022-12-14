87906aa9efa9dd74dd946c9c5b3cb70371f27349
zj565061763 <565061763@qq.com>
2022-12-14 18:23:01 +0800

    simplify

--- lib/src/main/java/com/sd/lib/compose/swiperefresh/RememberIndicatorContainerState.kt
+++ lib/src/main/java/com/sd/lib/compose/swiperefresh/RememberIndicatorContainerState.kt
@@ -5,74 +5,28 @@ import androidx.compose.runtime.remember
 
 @Composable
 internal fun rememberStartIndicatorContainerState(): IndicatorContainerState {
-    val swipeState = checkNotNull(LocalFSwipeRefreshState.current)
-    return when (swipeState.startIndicatorMode) {
-        IndicatorMode.Above -> rememberIndicatorContainerStateAbove(swipeState, RefreshDirection.Start)
-        IndicatorMode.Drag -> rememberIndicatorContainerStateDrag(swipeState, RefreshDirection.Start)
-        IndicatorMode.Below -> rememberIndicatorContainerStateBelow(swipeState, RefreshDirection.Start)
-        IndicatorMode.Boundary -> rememberIndicatorContainerStateBoundary(swipeState, RefreshDirection.Start)
+    val state = checkNotNull(LocalFSwipeRefreshState.current)
+    val mode = state.startIndicatorMode
+    return remember(state, mode) {
+        when (mode) {
+            IndicatorMode.Above -> IndicatorContainerStateAbove(state, RefreshDirection.Start)
+            IndicatorMode.Drag -> IndicatorContainerStateDrag(state, RefreshDirection.Start)
+            IndicatorMode.Below -> IndicatorContainerStateBelow(state, RefreshDirection.Start)
+            IndicatorMode.Boundary -> IndicatorContainerStateBoundary(state, RefreshDirection.Start)
+        }
     }
 }
 
 @Composable
 internal fun rememberEndIndicatorContainerState(): IndicatorContainerState {
-    val swipeState = checkNotNull(LocalFSwipeRefreshState.current)
-    return when (swipeState.endIndicatorMode) {
-        IndicatorMode.Above -> rememberIndicatorContainerStateAbove(swipeState, RefreshDirection.End)
-        IndicatorMode.Drag -> rememberIndicatorContainerStateDrag(swipeState, RefreshDirection.End)
-        IndicatorMode.Below -> rememberIndicatorContainerStateBelow(swipeState, RefreshDirection.End)
-        IndicatorMode.Boundary -> rememberIndicatorContainerStateBoundary(swipeState, RefreshDirection.End)
-    }
-}
-
-/**
- * [IndicatorContainerState] for [IndicatorMode.Above].
- */
-@Composable
-private fun rememberIndicatorContainerStateAbove(
-    swipeRefreshState: FSwipeRefreshState,
-    direction: RefreshDirection,
-): IndicatorContainerState {
-    return remember(swipeRefreshState, direction) {
-        IndicatorContainerStateAbove(swipeRefreshState, direction)
-    }
-}
-
-/**
- * [IndicatorContainerState] for [IndicatorMode.Drag].
- */
-@Composable
-private fun rememberIndicatorContainerStateDrag(
-    swipeRefreshState: FSwipeRefreshState,
-    direction: RefreshDirection,
-): IndicatorContainerState {
-    return remember(swipeRefreshState, direction) {
-        IndicatorContainerStateDrag(swipeRefreshState, direction)
-    }
-}
-
-/**
- * [IndicatorContainerState] for [IndicatorMode.Below].
- */
-@Composable
-private fun rememberIndicatorContainerStateBelow(
-    swipeRefreshState: FSwipeRefreshState,
-    direction: RefreshDirection,
-): IndicatorContainerState {
-    return remember(swipeRefreshState, direction) {
-        IndicatorContainerStateBelow(swipeRefreshState, direction)
-    }
-}
-
-/**
- * [IndicatorContainerState] for [IndicatorMode.Boundary].
- */
-@Composable
-private fun rememberIndicatorContainerStateBoundary(
-    swipeRefreshState: FSwipeRefreshState,
-    direction: RefreshDirection,
-): IndicatorContainerState {
-    return remember(swipeRefreshState, direction) {
-        IndicatorContainerStateBoundary(swipeRefreshState, direction)
+    val state = checkNotNull(LocalFSwipeRefreshState.current)
+    val mode = state.endIndicatorMode
+    return remember(state, mode) {
+        when (mode) {
+            IndicatorMode.Above -> IndicatorContainerStateAbove(state, RefreshDirection.End)
+            IndicatorMode.Drag -> IndicatorContainerStateDrag(state, RefreshDirection.End)
+            IndicatorMode.Below -> IndicatorContainerStateBelow(state, RefreshDirection.End)
+            IndicatorMode.Boundary -> IndicatorContainerStateBoundary(state, RefreshDirection.End)
+        }
     }
 }
