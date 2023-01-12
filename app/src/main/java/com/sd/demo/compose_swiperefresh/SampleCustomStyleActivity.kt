3d0b72a78ff3d2750096a9c1032fdb38c29bd9c9
zj565061763 <565061763@qq.com>
2023-01-12 19:46:10 +0800

    sync refresh state

--- app/src/main/java/com/sd/demo/compose_swiperefresh/SampleCustomStyleActivity.kt
+++ app/src/main/java/com/sd/demo/compose_swiperefresh/SampleCustomStyleActivity.kt
@@ -47,19 +47,14 @@ private fun Sample(
         it.endIndicatorMode = IndicatorMode.Drag
     }
 
-    LaunchedEffect(uiState.isRefreshing) {
-        state.refreshStart(uiState.isRefreshing)
-    }
-    LaunchedEffect(uiState.isLoadingMore) {
-        state.refreshEnd(uiState.isLoadingMore)
-    }
-
     LaunchedEffect(Unit) {
         viewModel.refresh(20)
     }
 
     FSwipeRefresh(
         state = state,
+        isRefreshingStart = uiState.isRefreshing,
+        isRefreshingEnd = uiState.isLoadingMore,
         onRefreshStart = {
             logMsg { "onRefreshStart" }
             viewModel.refresh(20)
