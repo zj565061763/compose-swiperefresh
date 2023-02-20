376e039cc907f511f9b8c69f0c5540011c40904f
zj565061763 <565061763@qq.com>
2023-02-20 20:57:33 +0800

    kts

---
+++ lib/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    `maven-publish`
+}
+
+val libGroupId = "com.sd.lib.android"
+val libArtifactId = "compose-swiperefresh"
+val libVersionName = "1.0.0-alpha06"
+
+android {
+    namespace ="com.sd.lib.compose.swiperefresh"
+    compileSdk = libs.versions.androidCompileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.androidMinSdk.get().toInt()
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+        kotlinOptions.freeCompilerArgs += "-module-name=$libGroupId.$libArtifactId"
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.composeCompiler.get()
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
+}
+
+dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.material3)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = libGroupId
+                artifactId = libArtifactId
+                version = libVersionName
+            }
+        }
+    }
+}
